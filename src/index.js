51686f9f38efd610aa61c1ba4fce6e4002ccc12e
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-03-15 15:35:10 +0200

    Add the popup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import './style.css';
+import renderPopup from './modules/renderPopup.js';
 import displayMeal from './modules/displayMeal.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   displayMeal();
+  renderPopup();
 });
